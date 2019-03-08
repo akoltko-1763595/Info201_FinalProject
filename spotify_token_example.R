82b5ae8dd0740db773ac601717966006ed626a50
Samantha Steele <steele38@uw.edu>
2019-03-07 16:50:04 -0800

    Add spotify token example

---
+++ spotify_token_example.R
@@ -0,0 +1,13 @@
+#install.packages("Rspotify")
+library(Rspotify)
+library(httr)
+library(jsonlite)
+
+
+# Use spotifyOAuth to gain authenticatoin
+# Client_id and client_secret on Spotify for Developers dashboard
+# Redirect URI needed to be whitelisted on Spotify for Developers dashboard, URI found in rejected redirect URI, decoded
+my_token <- spotifyOAuth(app_id="spotify_token",client_id = my_client_id,client_secret = my_client_secret)
+
+my_client_id <- "your-key-here"
+my_client_secret <- "your-key-here"
