f26f23e78e6b2b184f962eff5ee9703880552176
Samantha Steele <steele38@uw.edu>
2019-03-10 10:02:16 -0700

    Add Shiny files

---
+++ Info_Final_Shiny/server.R
@@ -0,0 +1,20 @@
+library(shiny)
+server <- function(input, output) {
+  
+  ## Question 1: Does Greatness Hold Up? (Sam)
+  
+  
+  
+  ## Question 2: What Makes an album great? (Andrew)
+  
+  
+  
+  
+  ## Question 3: What do fans and critics agree on? (Alex)
+  
+  
+  
+  
+  ## Quesiton 4: How well do sales dictate greatness? (Spencer)
+
+}
