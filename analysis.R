50b693f93a794a9f695ea6e0e4eb74a8df396fd6
spencerhh <spencer.hall16@gmail.com>
2019-03-10 19:03:13 -0700

    Add Q4

--- analysis.R
+++ analysis.R
@@ -68,7 +68,60 @@ popularity_comparison_data <- popularity_comparison_data %>%
 
 
 
-## Quesiton 4: How well do sales dictate greatness? (Spencer)
+## Question 4: How well do sales dictate greatness? (Spencer)
+library("openxlsx")
+library("dplyr")
+
+best_albums <- read.csv("data/RollingStonesTop500Albums.csv", stringsAsFactors = FALSE) %>%
+  select(Artist, Album, Year, Genre, Subgenre, Place)
+
+# Going to use the Probable sales data instead of Minimal or Range
+record_sales <- read.xlsx("data/CombinedRecordSales.xlsx") %>%
+  select(Artist, Album.Title, Probable) # Numbers are in millions
+colnames(record_sales)[2] <- "Album"
+
+# Combined the "best" albums with the best selling albums of all time
+# Of 300 possible overlapping albums, only 41 actually do
+# Of these 41, almost all of them are Rock and from the 80s and 90s
+combined_best_and_sales <- left_join(best_albums, record_sales, by = "Album") %>%
+  filter(!is.na(Probable)) %>%
+  select(Artist.x, Album, Year, Genre, Subgenre, Place, Probable)
+colnames(combined_best_and_sales)[1] <- "Artist"
+
+View(record_sales)
+View(best_albums)
+View(combined_best_and_sales)
+
+
+
+
+# Testing while I wait for someone to push to Shiny files
+# Need to fix the y-axis values because they're incredibly high for essentially no reason at all
+library("ggplot2")
+
+# Sales versus Rank
+ggplot(data = combined_best_and_sales, mapping = aes(Probable, Place)) +
+  geom_point(mapping = aes(color = Year)) +
+  geom_smooth() +
+  scale_y_continuous(limits = c(0, 500)) +
+  labs(
+    title = "Total Sales versus Album Rank", # plot title
+    x = "Probable Album Sales", # x-axis label
+    y = "Rolling Stones' Ranking", # y-axis label
+    color = "Year" # legend label for the "color" property
+  )
+
+# Year versus Rank
+ggplot(data = combined_best_and_sales, mapping = aes(Year, Place)) +
+  geom_col(mapping = aes(fill = Genre)) +
+  scale_x_continuous(breaks = combined_best_and_sales$Year) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  # theme(panel.grid.minor=element_blank(),
+        # panel.grid.major=element_blank())
+
+# Genre versus Sales
+ggplot(data = combined_best_and_sales, mapping = aes(Genre, Probable)) +
+  geom_col()
 
 
 
