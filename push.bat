ed1022d03c432924b49cc0a5be79efb692b64a5e
spencerhh <spencer.hall16@gmail.com>
2019-03-12 13:56:41 -0700

    .

--- push.bat
+++ push.bat
@@ -1,10 +1,11 @@
 @echo off
-git pull
-echo "Up to date, starting push"
-pause
 
 set /p msg="Enter Commit Message: "
 git add .
 git commit -m "%msg%"
+git pull
+echo "Up to date, close this window if merge issues else press space"
+pause
+
 git push
 pause
